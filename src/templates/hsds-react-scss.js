e0fcca900ee75ff1d0f2a025a05e5d9492cc20d3
Jon Q <hello@jonquach.com>
2019-02-14 09:38:45 -0500

    Update hsds-react-scss template

--- src/templates/hsds-react-scss.js
+++ src/templates/hsds-react-scss.js
@@ -7,34 +7,21 @@ const content = `
 @import 'pack/seed-color-scheme/_index';
 
 // Text
-@include _color(
-  (
-    blue:
-      (
-        default: ${latest.blue.default},
-        100: ${latest.blue['100']},
-        200: ${latest.blue['200']},
-        300: ${latest.blue['300']},
-        400: ${latest.blue['400']},
-        500: ${latest.blue['500']},
-        600: ${latest.blue['600']},
-        700: ${latest.blue['700']},
-        800: ${latest.blue['800']},
-        900: ${latest.blue['900']},
-      ),
-    note: (background: #fff7dd),
-    text:
-      (
-        default: _color(charcoal, 700),
-        body: _color(charcoal, 400),
-        heading: _color(charcoal, 700),
-        subtle: _color(charcoal, 500),
-        muted: _color(charcoal, 300),
-        faint: _color(charcoal, 200),
-        extraMuted: _color(grey, 600)
-      )
-  )
-);
+@include _color((
+  blue:
+    (
+      default: ${latest.blue.default},
+      100: ${latest.blue['100']},
+      200: ${latest.blue['200']},
+      300: ${latest.blue['300']},
+      400: ${latest.blue['400']},
+      500: ${latest.blue['500']},
+      600: ${latest.blue['600']},
+      700: ${latest.blue['700']},
+      800: ${latest.blue['800']},
+      900: ${latest.blue['900']},
+    ),
+));
 `
 
 generate({
