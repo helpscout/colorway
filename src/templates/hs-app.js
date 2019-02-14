f20b56533045179a312bdb52745ac2409a7100f0
Jon Q <hello@jonquach.com>
2019-02-14 13:03:00 -0500

    Add HS App manifest

---
+++ src/templates/hs-app.js
@@ -0,0 +1,82 @@
+const { generate } = require('../generate')
+const config = require('../configs').getColors()
+const { previous, latest } = config
+
+const generateLatestColorScheme = () => {
+  let output = ''
+  const targets = ['blue']
+
+  Object.keys(latest).forEach(color => {
+    if (targets.includes(color)) {
+      const shades = latest[color]
+      output += `
+        ${color}: (
+          ${JSON.stringify(shades, null, 2).replace(/{|}|"/g, '')}
+        ),
+      `
+    }
+  })
+
+  return output
+}
+const generatePreviousColorScheme = () => {
+  let output = ''
+  const targets = [
+    'charcoal',
+    'grey',
+    'yellow',
+    'green',
+    'red',
+    'purple',
+    'orange',
+  ]
+
+  Object.keys(previous).forEach(color => {
+    if (targets.includes(color)) {
+      const shades = previous[color]
+      output += `
+        ${color}: (
+          ${JSON.stringify(shades, null, 2).replace(/{|}|"/g, '')}
+        ),
+      `
+    }
+  })
+
+  return output
+}
+
+const content = `
+$seed-color-scheme-helpscout: (
+  white: #fff,
+  black: #000,
+
+  ${generateLatestColorScheme()}
+  ${generatePreviousColorScheme()}
+
+  text: (
+    default: #2b2b2b
+  ),
+
+  button: (
+    border: #d5d5d5,
+    hover: #e6e6e6,
+    active: #b6b6b6
+  ),
+
+  link: (
+    default: #0077CC,
+    base: #0077CC,
+    hover: #1292EE,
+    active: #1292EE,
+    focus: #1292EE
+  )
+);
+
+@import "pack/seed-color-scheme-helpscout/_index";
+`.trim()
+
+generate({
+  name: 'hs-app',
+  dest: 'site/scss/config/_seed-color-scheme-helpscout.scss',
+  content,
+})
