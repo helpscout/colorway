005d8e7ce099a404cdb3881336443e13c865c761
Pierre-Luc Babin <plbabin@gmail.com>
2019-07-17 21:30:59 +0000

    0.2.2

--- dist/hsds-react/src/styles/configs/colorway.ts
+++ dist/hsds-react/src/styles/configs/colorway.ts
@@ -1,4 +1,4 @@
-// Generated with @helpscout/colorway (v0.2.1)
+// Generated with @helpscout/colorway (v0.2.2)
 //
 // This file was automatically generated with @helpscout/colorway
 // Please don't modify this file. Your changes will be overwritten.
