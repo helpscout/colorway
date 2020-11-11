60e8f68d7abc2138e70242775f3adef11299c415
Pierre-Luc Babin <plbabin@gmail.com>
2020-11-11 16:26:51 +0000

    0.8.0

--- dist/seed-color-scheme/packages/seed-color-scheme/scss/pack/seed-color-scheme/_config.scss
+++ dist/seed-color-scheme/packages/seed-color-scheme/scss/pack/seed-color-scheme/_config.scss
@@ -1,4 +1,4 @@
-// Generated with @helpscout/colorway (v0.7.0)
+// Generated with @helpscout/colorway (v0.8.0)
 //
 // This file was automatically generated with @helpscout/colorway
 // Please don't modify this file. Your changes will be overwritten.
