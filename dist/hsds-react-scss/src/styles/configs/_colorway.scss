a490fd70364bba6978cc93e1a9448c926b85a238
Pierre-Luc Babin <plbabin@gmail.com>
2020-11-26 15:36:29 +0000

    0.9.2

--- dist/hsds-react-scss/src/styles/configs/_colorway.scss
+++ dist/hsds-react-scss/src/styles/configs/_colorway.scss
@@ -1,4 +1,4 @@
-// Generated with @helpscout/colorway (v0.9.1)
+// Generated with @helpscout/colorway (v0.9.2)
 //
 // This file was automatically generated with @helpscout/colorway
 // Please don't modify this file. Your changes will be overwritten.
