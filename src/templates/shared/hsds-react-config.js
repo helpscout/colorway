6d2770cdf20e88239861b856c2bfc3007ef4204a
Pierre-Luc Babin <pl@helpscout.com>
2020-11-26 09:46:38 -0500

    Upgrade green color. Add pink to hsds, hs-app and search autocomplete (#15)

--- src/templates/shared/hsds-react-config.js
+++ src/templates/shared/hsds-react-config.js
@@ -5,9 +5,11 @@ exports.colorConfig = {
   ash: config.latest.ash,
   blue: config.latest.blue,
   charcoal: config.latest.charcoal,
+  green: config.latest.grey,
   grey: config.latest.grey,
   indigo: config.latest.indigo,
   lavender: config.latest.lavender,
+  pink: config.latest.pink,
   whaletail: config.latest.whaletail,
   yellow: config.latest.yellow,
 }
