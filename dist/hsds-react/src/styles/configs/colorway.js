83a6000b4b10938df872656c65568ca127724fb6
Pierre-Luc Babin <plbabin@gmail.com>
2021-09-28 18:08:36 +0000

    0.9.6

--- dist/hsds-react/src/styles/configs/colorway.js
+++ dist/hsds-react/src/styles/configs/colorway.js
@@ -1,4 +1,4 @@
-// Generated with @helpscout/colorway (v0.9.5)
+// Generated with @helpscout/colorway (v0.9.6)
 //
 // This file was automatically generated with @helpscout/colorway
 // Please don't modify this file. Your changes will be overwritten.
